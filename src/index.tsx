0731a0fd94f4e6de1e1e2ce34c47bbe7d79a73ec
Gabriel <rabeloglr@gmail.com>
2021-06-04 19:44:07 -0300

    refactor: add example tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
