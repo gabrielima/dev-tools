52072d1f103733cc363372559215585cf4c77508
Gabriel <rabeloglr@gmail.com>
2021-06-05 01:19:48 -0300

    feat: add CpfService

---
+++ src/services/cpf.service.ts
@@ -0,0 +1,27 @@
+export default class CpfService {
+
+  static validate(cpf: string): boolean {
+    cpf = cpf.replaceAll(/^D/g, '')
+
+    if (cpf.length !== 11)
+      return false
+
+    return true
+  }
+
+  static generate(formatted?: boolean): string {
+    let generatedCpf = '00000000003'
+
+    if (formatted)
+      return this.format(generatedCpf)
+
+    return generatedCpf
+  }
+
+  static format(cpf: string): string {
+    if (!this.validate(cpf))
+      return cpf;
+
+    return cpf.replace(/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/, '$1.$2.$3-$4')
+  }
+}
