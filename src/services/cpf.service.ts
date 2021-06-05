124ebbb09ab1b3bf02c7cc410f5ab4c7ddd6c5ea
Gabriel <rabeloglr@gmail.com>
2021-06-05 01:30:28 -0300

    refactor: prettier rules on all files

--- src/services/cpf.service.ts
+++ src/services/cpf.service.ts
@@ -1,10 +1,8 @@
 export default class CpfService {
-
   static validate(cpf: string): boolean {
     cpf = cpf.replaceAll(/^D/g, '')
 
-    if (cpf.length !== 11)
-      return false
+    if (cpf.length !== 11) return false
 
     return true
   }
@@ -12,16 +10,17 @@ export default class CpfService {
   static generate(formatted?: boolean): string {
     let generatedCpf = '00000000003'
 
-    if (formatted)
-      return this.format(generatedCpf)
+    if (formatted) return this.format(generatedCpf)
 
     return generatedCpf
   }
 
   static format(cpf: string): string {
-    if (!this.validate(cpf))
-      return cpf;
+    if (!this.validate(cpf)) return cpf
 
-    return cpf.replace(/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/, '$1.$2.$3-$4')
+    return cpf.replace(
+      /([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/,
+      '$1.$2.$3-$4'
+    )
   }
 }
